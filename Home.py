8412ce1cf090e532cb8ae803903798b41822cdca
BaltasarMR <89664366+BaltasarMR@users.noreply.github.com>
2023-02-03 17:58:06 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -122,7 +122,7 @@ if check_password():
         
         if FazerBackup:
             try:
-                os.system("""ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts""")                         
+                os.system("""echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config""")                         
                 os.system("""git config --global user.email 'baltasarmatheus@hotmail.com' """)         
                 os.system("""git config --global user.name 'BaltasarMR' """)                            
                 os.system("git add pages")
